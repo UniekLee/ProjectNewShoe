91fcf8e80f5daca2ae30f6c7c43f5b2d87541291
Lee Watkins <leewatkins@monzo.com>
2021-06-15 19:31:03 +0100

    Initial Commit

---
+++ ProjectNewShoe/ProjectNewShoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectNewShoeApp.swift
+//  ProjectNewShoe
+//
+//  Created by Lee Watkins on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectNewShoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
