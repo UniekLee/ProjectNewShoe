33b6964c7130b8a99022bf0bf6a86dc16b8cfd42
Lee Watkins <leewatkins@monzo.com>
2021-07-03 16:30:11 +0100

    Got the project building and running

---
+++ Project.swift
@@ -0,0 +1,48 @@
+import ProjectDescription
+
+let project = Project(
+    name: "ProjectNewShoe",
+    packages: [
+        .remote(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            requirement: .upToNextMajor(from: "0.20.0")
+        ),
+    ],
+    targets: [
+        Target(
+            name: "ProjectNewShoe",
+            platform: .iOS,
+            product: .app,
+            bundleId: "com.unieklee.ProjectNewShoe",
+            infoPlist: "Config/ProjectNewShoe.plist",
+            sources: [
+                "Sources/**"
+            ],
+            resources: [
+                "Resources/**"
+            ],
+            entitlements: "Config/ProjectNewShoe.plist",
+            dependencies: [
+                .sdk(name: "HealthKit.framework", status: .required),
+//                .package(product: "swift-composable-architecture")
+            ]
+        ),
+        Target(
+            name: "ProjectNewShoe_UnitTests",
+            platform: .iOS,
+            product: .unitTests,
+            bundleId: "com.unieklee.PNSUnitTests",
+            infoPlist: "UnitTests/Info.plist",
+            sources: [
+                "UnitTests/Sources/**"
+            ],
+//            resources: [
+//                "UnitTests/Resources/**"
+//            ],
+            dependencies: [
+                .xctest,
+                .target(name: "ProjectNewShoe")
+            ]
+        )
+    ]
+)
